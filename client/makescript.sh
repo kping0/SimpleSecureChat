35cd5d111583169238027e62e3a57370130dfd01
kping0 <36621476+kping0@users.noreply.github.com>
2018-03-09 02:08:24 -0500

    Update makescript.sh

--- client/makescript.sh
+++ client/makescript.sh
@@ -6,4 +6,4 @@ cd ../../
 clear
 echo "Compiling Main Application"
 rm -f testbinary
-gcc -o testbinary sec_chat_client.c -s -Os -Wall -Wextra -lssl -lcrypto -lbinn && chmod +x testbinary && strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id testbinary
+gcc -o testbinary secure_chat.c -s -Os -Wall -Wextra -lssl -lcrypto -lbinn && chmod +x testbinary && strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id testbinary

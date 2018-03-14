f4c228ea9db02cf8aea3becb95fd8b45bf2d1b8a
kping0 <36621476+kping0@users.noreply.github.com>
2018-03-13 23:09:01 -0400

    Update makescript.sh

--- client/makescript.sh
+++ client/makescript.sh
@@ -5,6 +5,7 @@
 #sudo make install #install binn
 #cd ../../
 sudo apt-get install libssl-dev # sudo yum install openssl-devel on fedora
+sudo apt-get install libsqlite3-dev
 git clone https://github.com/liteserver/binn
 cd binn
 make 
@@ -13,4 +14,4 @@ cd ..
 clear
 echo "Compiling Main Application"
 rm -f testbinary
-gcc -o testbinary secure_chat.c -s -Os -Wall -Wextra -lssl -lcrypto -lbinn && chmod +x testbinary && strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id testbinary
+gcc -o testbinary secure_chat.c -s -Os -Wall -Wextra -lssl -lcrypto -lbinn -lsqlite3 && chmod +x testbinary && strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id testbinary

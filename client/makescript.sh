facd3a1b5b5703e18706693a55895f2cccb6cae9
kping0 <36621476+kping0@users.noreply.github.com>
2018-03-15 16:08:13 -0400

    compile , seperate link

--- client/makescript.sh
+++ client/makescript.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-sudo apt-get install libssl-dev
-sudo apt-get install libsqlite3-dev
+rm -f testbinary
+sudo apt-get install libssl-dev libsqlite3-dev git gcc 
 git clone https://github.com/liteserver/binn
-cd binn
-make 
+cd binn/
+make
 sudo make install
 cd ..
 clear
 echo "Compiling Main Application"
-rm -f testbinary
-gcc -o testbinary secure_chat.c -s -Os -Wall -Wextra -lssl -lcrypto -lbinn -lsqlite3 && strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id testbinary
+gcc -o sscssl.o -c headers/sscssl.c -Os -s -Wall -Wextra -lssl -lcrypto -static
+gcc -o sscasymmetric.o -c headers/sscasymmetric.c -Os -s -Wall -Wextra -lssl -lcrypto -static
+gcc -o sscdbfunc.o -c headers/sscdbfunc.c -Os -s -Wall -Wextra -lssl -lcrypto -lsqlite3 -static
+gcc -o main.o -c sec_chat_client.c -Os -s -Wall -Wextra -lssl -lcrypto -lsqlite3 -static
+gcc -o testbinary main.o sscssl.o sscasymmetric.o sscdbfunc.o -Os -s -Wall -Wextra -lssl -lcrypto -lsqlite3 
+rm -f sscssl.o sscasymmetric.o sscdbfunc.o main.o 

832bcbda4fa9fa18500ce7afd64c26117053c7da
kping0 <36621476+kping0@users.noreply.github.com>
2018-03-18 18:18:47 -0400

    Add files via upload

--- client/sec_chat_client.c
+++ client/sec_chat_client.c
@@ -14,12 +14,12 @@
 #include <openssl/bio.h>
 #include <openssl/pem.h>
 #include <sqlite3.h> 
-#include <binn.h>
 
-//Custom Functions
+#include "headers/binn.h" //Binn library 
 #include "headers/sscssl.h" //Connection functions
 #include "headers/sscasymmetric.h" //keypair functions
 #include "headers/sscdbfunc.h" //DB manipulation functions & B64
+
 /*
 * Application Settings
 */
@@ -29,18 +29,8 @@
 #define HOST_CERT "public.pem" // Server public certificate (X509 Public Cert)
 #define PUB_KEY "rsapublickey.pem" //Public Key location (Will be generated if not found)
 #define PRIV_KEY "rsaprivatekey.pem" //Private Key location (Will be generated if not found)
+#define KEYSIZE 8192 //keysize used to generate key (has to be 1024,2048,4096,or 8192)
 #define DB_FNAME "sscdb.db" //SQLITE Database Filename
-
-
-#ifndef sscsslstruct
-#define sscsslstruct
-struct ssl_str{ 
-	BIO *bio_obj;
-	SSL *ssl_obj;
-	SSL_CTX *ctx;
-	const SSL_METHOD *sslmethod;
-	};
-#endif
 
 const char* encryptmsg(char* username,unsigned char* message,sqlite3* db){ //returns b64 of binnobj that includes b64encryptedaeskey,aeskeylength,b64encrypedbuffer,encbuflen,b64iv,ivlen
 	if(strlen((const char*)message) > 1024){
@@ -102,19 +92,12 @@ const char* decryptmsg(const char *encrypted_buffer,EVP_PKEY* privKey){ // Attem
 	}	
 	
 	int enc_len = binn_object_int32(obj,"enc_len");
-
 	unsigned char* enc_buf = binn_object_blob(obj,"enc_buf",&enc_len);
-
 	int ekl = binn_object_int32(obj,"ekl");
-	
 	unsigned char* ek = binn_object_blob(obj,"ek",&ekl);
-	
 	int iv_len = binn_object_int32(obj,"iv_len");
-	
 	unsigned char* iv = binn_object_blob(obj,"iv",&iv_len);	
-	
 	unsigned char* dec_buf = malloc(2000);
-	
 	memset(dec_buf,0,2000);
 	
 	int dec_len = envelope_open(privKey,enc_buf,enc_len,ek,ekl,iv,dec_buf);
@@ -122,6 +105,7 @@ const char* decryptmsg(const char *encrypted_buffer,EVP_PKEY* privKey){ // Attem
 		puts("Error Decrypting error");
 		return NULL;
 	}
+
 	char *f_buf = malloc(dec_len);
 	memset(f_buf,0,dec_len);
 	memcpy(f_buf,dec_buf,dec_len-1);
@@ -138,7 +122,7 @@ int main(void){
 	//Setup SSL Connection
 	struct ssl_str *tls_vars = malloc(sizeof(struct ssl_str));
 	if(TLS_conn(tls_vars,HOST_CERT,HOST_NAME,HOST_PORT)){ /*function that creates a TLS connection & alters the struct(ssl_str)ssl_o*/
-		BIO_write(tls_vars->bio_obj,"12345",5);
+		//BIO_write(tls_vars->bio_obj,"12345 this is a more complicated message",40);
 		puts("SSL/TLS OK");
 		puts("Connected to " HOST_NAME ":" HOST_PORT " using server-cert: " HOST_CERT);
 	}
@@ -149,10 +133,10 @@ int main(void){
 	EVP_PKEY* pubk_evp = EVP_PKEY_new();
 	EVP_PKEY* priv_evp = EVP_PKEY_new();
 	if(!LoadKeyPair(pubk_evp,priv_evp,PUB_KEY,PRIV_KEY)){
-		puts("Loaded Keypair ERROR\nGenerating new keypair OK");
+		printf("Loaded Keypair ERROR\nGenerating %i bit Keypair, this can take up to 5 minutes!\n",KEYSIZE);
 		EVP_PKEY_free(pubk_evp);
 		EVP_PKEY_free(priv_evp);
-		CreateKeyPair(PUB_KEY,PRIV_KEY);
+		CreateKeyPair(PUB_KEY,PRIV_KEY,KEYSIZE);
 		puts("Please restart the binary to load your keypair");
 		return 0;
 		
@@ -174,24 +158,25 @@ int main(void){
 		puts("Usercheck ERROR");
 		goto CLEANUP;
 	}
-/*
+
+				//Sample code for Send/Recv	
 	char msg2test[200];
 	char* decbuf;
+	char* encbuf;
 	while(1){
-		memset(msg2test,0,200);
+		memset(msg2test,0,sizeof(msg2test));
+		printf("Message to send to \"user\": ");
 		fgets(msg2test,200,stdin);
-		decbuf = (char*)decryptmsg(
-			encryptmsg("user",(unsigned char*)msg2test,db), // <-- This part would be done by the other person to encrypt the message, 
-			priv_evp); // <-- private key to use to decrypt message
-		if(decbuf != NULL){		
-			puts(decbuf);
-		}
-		else{
-			puts("error decrypting message.");
-			goto CLEANUP;		
-		}
+		//sending user
+		encbuf = (char*)encryptmsg("user",(unsigned char*)msg2test,db); //"user" would be the receiving username
+		printf("Encrypted message: %s\n",encbuf);
+
+		//receiving user 
+		decbuf = (char*)decryptmsg(encbuf,priv_evp); // decrypt
+		if(decbuf == NULL) goto CLEANUP;		
+		printf("Decrypted Message: %s\n",decbuf); 
 	}
-	*/
+	
 CLEANUP:
 	
 	puts("Cleaning up Objects...");	

9faa53fe66570d0e86738b5a87a11bd6b341bc58
kping0 <36621476+kping0@users.noreply.github.com>
2018-04-01 02:29:44 -0400

    move functions and fix authentication

--- server/headers/sscsrvfunc.h
+++ server/headers/sscsrvfunc.h
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <arpa/inet.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
@@ -20,6 +21,13 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h> 
 
+#include "binn.h"
+#include "settings.h"
+
+extern int sock;
+
+extern volatile int gsigflag;
+
 int create_socket(int port);
 
 void init_openssl(void);
@@ -29,5 +37,29 @@ void cleanup_openssl(void);
 SSL_CTX *create_context(void);
 
 void configure_context(SSL_CTX* ctx);
+
+int checkforUser(char* username,sqlite3* db); 
+
+int addUser2DB(char* username,char* b64rsa,int rsalen,char* authkey,sqlite3* db);
+	
+char *base64encode (const void *b64_encode_this, int encode_this_many_bytes);
+
+char *base64decode (const void *b64_decode_this, int decode_this_many_bytes);
+
+void sig_handler(int sig);
+
+void childexit_handler(int sig);
+
+int getUserUID(char* username,sqlite3 *db);
+
+int AddMSG2DB(sqlite3* db,char* recipient,unsigned char* message);
+
+sqlite3* initDB(char* dbfname);
+
+const char* GetEncodedRSA(char* username, sqlite3* db);
+
+char* GetUserMessagesSRV(char* username,sqlite3* db);
+
+char* getUserAuthKey(char* username, sqlite3* db); //gets authkey of user 'username', used for authentication
 
 #endif
